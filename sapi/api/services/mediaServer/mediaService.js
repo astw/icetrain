10d030863a0568cd65c51c9a2f0aa42b3d946322
Shuhao Wang <wshuhao@gmail.com>
2015-06-10 23:17:00 -0700

    play stream video

--- sapi/api/services/mediaServer/mediaService.js
+++ sapi/api/services/mediaServer/mediaService.js
@@ -20,7 +20,13 @@ module.exports =  {
        // console.log(mediaInfo);
        //var url = mediaInfo.path;
 
+       var range = req.headers.range;
+       var positions = range.replace(/bytes=/,"").split("-");
+       var start =parseInt(positions[0],10);
+
+       console.log(range);
        var data = cache[token];
+       data = null;
        if (data == null) {
          Video.findOne({urltoken: token}).then(function (video) {
            var mediaPath = video.path;
@@ -30,13 +36,33 @@ module.exports =  {
            var mediaPath= path.join(root,mediaPath);
            console.log( mediaPath );
 
-           fs.readFile(mediaPath, function (err, data) {
+         //  fs.readFile(mediaPath, function (err, data) {
+           fs.stat(mediaPath, function(err, stats) {
              if (err) {
                throw err;
              }
-             cache[token] = data;
-             res.writeHead(200, {'Content-Length': data.length, 'Content-Type': 'video/mp4'});
-             res.end(data);
+
+             var total = stats.size;
+             var end = positions[1] ? parseInt(positions[1],10) : total -1;
+             var chunksize = (end - start) + 1;
+
+             res.writeHead(206, {
+               "Content-Range": "bytes " + start + "-" + end + "/" + total,
+               "Accept-Ranges": "bytes",
+               "Content-Length": chunksize,
+               "Content-Type": "video/mp4"
+             });
+
+             var stream = fs.createReadStream(mediaPath, { start: start, end: end })
+               .on("open", function() {
+                 stream.pipe(res);
+               }).on("error", function(err) {
+                 res.end(err);
+               });
+
+             //cache[token] = data;
+             //res.writeHead(200, {'Content-Length': data.length, 'Content-Type': 'video/mp4'});
+             //res.end(data);
            });
          })
        }
